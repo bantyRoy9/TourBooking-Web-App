bb7a359b038a7159679870bf9bc01ce9b2dd51fc
Banty <mr.bantikumar9716@gmail.com>
2024-03-10 19:49:04 +0530

    update server for self runner

--- server.js
+++ server.js
@@ -7,14 +7,12 @@ const environment = process.env.NODE_ENV;
 dotenv.config({ path:'./config.env' })
 
 const DB = process.env.DATABASE_URL
-// const DB = process.env.DATABASE_LOCAL
 mongoose.connect(DB).then(con=> {
-    // console.log(con.connection);
     console.log('database is connected');
 });
 
 
 const PORT =process.env.PORT || 8000;
-app.listen(PORT,()=>{console.log(`server running on port ${PORT}`);})
+app.listen(PORT,()=>{console.log(`server running on port ${PORT}`,environment);})
 
 // console.log('work')
