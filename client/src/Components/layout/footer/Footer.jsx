2ef27543e1084b0c23c2df1d86364a2d2829d73d
Banty <mr.bantikumar9716@gmail.com>
2024-03-29 08:40:54 +0530

    update tour card css

--- client/src/Components/layout/footer/Footer.jsx
+++ client/src/Components/layout/footer/Footer.jsx
@@ -1,92 +1,166 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './footer.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.css";
 const Footer = () => {
   return (
-      <>
-    <div className='footer-container'>
+    <>
+      <div className="footer-container">
         <hr />
         <div className="footer">
-            <div className="explore">
-                <strong>Destinations</strong>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                
-            </div>
-            <div className="popular">
+          <div className="explore">
+            <strong>Destinations</strong>
+            <Link to={""}>
+              <p>Europe</p>
+            </Link>
+            <Link to={""}>
+              <p>North America</p>
+            </Link>
+            <Link to={""}>
+              <p>Pacific</p>
+            </Link>
+            <Link to={""}>
+              <p>South America</p>
+            </Link>
+            <Link to={""}>
+              <p>Asia</p>
+            </Link>
+            <Link to={""}>
+              <p>Africa</p>
+            </Link>
+            <Link to={""}>
+              <p>Middle East</p>
+            </Link>
+            <Link to={""}>
+              <p>Carribbean</p>
+            </Link>
+            <Link to={""}>
+              <p>Australia</p>
+            </Link>
+            <Link to={""}>
+              <p>More</p>
+            </Link>
+          </div>
+          <div className="popular">
             <strong>Cities</strong>
-               <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                
-            </div>
-            <div className="calculator">
+            <Link to={""}>
+              <p> Berlin</p>
+            </Link>
+            <Link to={""}>
+              <p>London</p>
+            </Link>
+            <Link to={""}>
+              <p>Barcelona</p>
+            </Link>
+            <Link to={""}>
+              <p>New York</p>
+            </Link>
+            <Link to={""}>
+              <p>Tokyo</p>
+            </Link>
+            <Link to={""}>
+              <p>Sao Paolo</p>
+            </Link>
+            <Link to={""}>
+              <p>Moscow</p>
+            </Link>
+            <Link to={""}>
+              <p>Belgrade</p>
+            </Link>
+            <Link to={""}>
+              <p>Zagreb</p>
+            </Link>
+            <Link to={""}>
+              <p>More</p>
+            </Link>
+          </div>
+          <div className="calculator">
             <strong>Interests</strong>
-            <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                
-            </div>
-            <div className="others">
+            <Link to={""}>
+              <p>Adventure</p>
+            </Link>
+            <Link to={""}>
+              <p>Family Holidays</p>
+            </Link>
+            <Link to={""}>
+              <p>Festivals</p>
+            </Link>
+            <Link to={""}>
+              <p>Wildlife</p>
+            </Link>
+            <Link to={""}>
+              <p>Cruises</p>
+            </Link>
+            <Link to={""}>
+              <p>Backpacking</p>
+            </Link>
+            <Link to={""}>
+              <p>Faraway Travels</p>
+            </Link>
+            <Link to={""}>
+              <p>Honeymoon and Romancing</p>
+            </Link>
+            <Link to={""}>
+              <p>Budget Travels</p>
+            </Link>
+            <Link to={""}>
+              <p>More</p>
+            </Link>
+          </div>
+          <div className="others">
             <strong>About</strong>
-            <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                
-            </div>
-            <div className="rate">
+            <Link to={""}>
+              <p>lorem</p>
+            </Link>
+            <Link to={""}>
+              <p>About Travelicious</p>
+            </Link>
+            <Link to={""}>
+              <p>Our Guides</p>
+            </Link>
+            <Link to={""}>
+              <p>Tour prices</p>
+            </Link>
+            <Link to={""}>
+              <p>FAQs</p>
+            </Link>
+            <Link to={""}>
+              <p>Contact us</p>
+            </Link>
+            <Link to={""}>
+              <p>Location</p>
+            </Link>
+            <Link to={""}>
+              <p>Testimonials</p>
+            </Link>
+            <Link to={""}>
+              <p>Clients</p>
+            </Link>
+          </div>
+          <div className="rate">
             <strong>Shop</strong>
-            <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                
-            </div>
-            <div className="rate">
-            <strong>newsletter</strong>
-            <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                <Link to={''}><p>lorem</p></Link>
-                
-            </div>
+            <Link to={""}>
+              <p>Shop</p>
+            </Link>
+            <Link to={""}>
+              <p>Mugs</p>
+            </Link>
+            <Link to={""}>
+              <p>T Shirts</p>
+            </Link>
+            <Link to={""}>
+              <p>Backpacks</p>
+            </Link>
+            <Link to={""}>
+              <p>Guides</p>
+            </Link>
+            <Link to={""}>
+              <p>Books</p>
+            </Link>
+          </div>
         </div>
       </div>
-      </>
-  )
-}
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- client/src/Views/PageNotFound/Page404/Page404.jsx
+++ client/src/Views/PageNotFound/Page404/Page404.jsx
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
-class Page404 extends Component {
-  render() {
-    return (
-      <div className="acessDenied"><div><span style={{fontSize:"55px"}}>404</span><br/><br/>Page Still In Development!</div></div>
-    );
-  }
-}
+import React from "react";
+const Page404 = () => {
+  return (
+    <div className="acessDenied">
+      <div>
+        <span style={{ fontSize: "55px" }}>404</span>
+        <br />
+        <br />
+        Page Still In Development!
+      </div>
+    </div>
+  );
+};
 
 export default Page404;
